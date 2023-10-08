34f11dabcb01a806599dd5bc68e3fd0bbccbde3b
sowmik-sec <sowmik.sec@gmail.com>
2023-10-08 12:11:12 +0600

    custom application properties

--- src/main/java/com/sowmik/springbootdemo/rest/FunRestController.java
+++ src/main/java/com/sowmik/springbootdemo/rest/FunRestController.java
@@ -1,10 +1,22 @@
 package com.sowmik.springbootdemo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${coach.team}")
+    private  String teamName;
+
+    @GetMapping("/teaminfo")
+    public String teaminfo(){
+        return "Team name: " + teamName + " coach name: " + coachName;
+    }
     @GetMapping("/")
     public String hay(){
         return "Hello World";
